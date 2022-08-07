a34005b0c4ae6acd47c9e20272748d4a5c19d7d6
chsergei03 <chsergey1003@gmail.com>
2022-08-07 22:09:35 +0300

    add tagsParsing module

---
+++ modules/tagsParsing.py
@@ -0,0 +1,57 @@
+import mutagen
+
+from mutagen import id3
+from mutagen.mp3 import MP3
+from mutagen.flac import FLAC
+
+# возвращает словарь тегов, присвоенных музыкальной композиции,
+# файл которой расположен по пути filepath.
+def getTagsDict(filepath):
+    trackFormat = filepath[-4:]
+
+    if trackFormat == ".mp3":
+        return MP3(filepath)
+    elif trackFormat == "flac":
+        return FLAC(filepath)
+
+# возвращает словарь ключей словаря тегов музыкальной композиции.
+def getKeysDict(tagsDict):
+    if isinstance(tagsDict, mutagen.flac.FLAC):
+        titleKey = 'TITLE'
+        albumKey = 'ALBUM'
+        artistKey = 'ARTIST'
+        albumArtistKey = 'ALBUMARTIST'
+        dateKey = 'DATE'
+        genreKey = 'GENRE'
+        composerKey = 'COMPOSER'
+    elif isinstance(tagsDict, mutagen.mp3.MP3):
+        titleKey = 'TIT2'
+        albumKey = 'TALB'
+        artistKey = 'TPE1'
+        albumArtistKey = 'TPE2'
+        dateKey = 'TDRC'
+        genreKey = 'TCON'
+        composerKey = 'TCOM'
+
+    keysDict = {
+        'title': titleKey,
+        'album': albumKey,
+        'artist': artistKey,
+        'albumArtist': albumArtistKey,
+        'date': dateKey,
+        'genre': genreKey,
+        'composer': composerKey
+    }
+
+    return keysDict
+
+# возвращает информацию о музыкальной композиции по ключу из словаря
+# тегов в формате строки.
+def getInfoFromTagsDictByKey(tagsDict, key):
+    keysDict = getKeysDict(tagsDict)
+    tag = tagsDict[keysDict[key]][0]
+
+    if isinstance(tag, mutagen.id3._specs.ID3TimeStamp):
+        tag = str(tag)
+
+    return tag
---
+++ tests/tagsParsing_tests/tagsParsing_tests.py
@@ -0,0 +1,202 @@
+import os
+import enum
+import mutagen
+
+import modules.tagsParsing as tagsParsing
+
+class testsErrorMessages(enum.Enum):
+    getKeysDict_errorMessage = "tags dictionary was not received"
+
+def getTagsDict_test_01():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Calvin Harris - Lean On Me "
+                               "(feat. Swae Lee).flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+
+    assert isinstance(tagsDict, mutagen.flac.FLAC)
+
+def getTagsDict_test_02():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Markul - 2 минуты.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+
+    assert isinstance(tagsDict, mutagen.mp3.MP3)
+
+def getTagsDict_test_03():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "half·alive - Hot Tea.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+
+    assert isinstance(tagsDict, mutagen.flac.FLAC)
+
+def getTagsDict_test_04():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Куок - Миллион мегатонн.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+
+    assert isinstance(tagsDict, mutagen.flac.FLAC)
+
+def getTagsDict_test_05():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Thrice - Scavengers.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+
+    assert isinstance(tagsDict, mutagen.flac.FLAC)
+
+def getTagsDict_tests():
+    getTagsDict_test_01()
+    getTagsDict_test_02()
+    getTagsDict_test_03()
+    getTagsDict_test_04()
+    getTagsDict_test_05()
+
+def getInfoFromTagsDictByKey_test_title_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Markul, Тося Чайкина - Стрелы.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    title = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'title')
+
+    assert title == "Стрелы"
+
+def getInfoFromTagsDictByKey_test_title_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Calvin Harris - Nothing More To Say (feat. 6LACK & Donae’O).flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    title = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'title')
+
+    assert title == "Nothing More To Say (feat. 6LACK & Donae’O)"
+
+def getInfoFromTagsDictByKey_test_album_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Idan - Комната. вход.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    album = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'album')
+
+    assert album == "Зона комфорта"
+
+def getInfoFromTagsDictByKey_test_album_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Ed Sheeran - 2step.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    album = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'album')
+
+    assert album == "="
+
+def getInfoFromTagsDictByKey_test_artist_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "LeTai - Миядзаки.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    artist = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'artist')
+
+    assert artist == "LeTai"
+
+def getInfoFromTagsDictByKey_test_artist_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Mike Shinoda - Make It Up As I Go (feat. K.Flay).flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    artist = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'artist')
+
+    assert artist == "Mike Shinoda"
+
+def getInfoFromTagsDictByKey_test_albumArtist_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Jeembo - Следуй за мной.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    albumArtist = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'albumArtist')
+
+    assert albumArtist == "Jeembo"
+
+def getInfoFromTagsDictByKey_test_albumArtist_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Markul - Карусель.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    albumArtist = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'albumArtist')
+
+    assert albumArtist == "Markul"
+
+def getInfoFromTagsDictByKey_test_date_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Loqiemean - Солнечная Сторона.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    date = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'date')
+
+    assert date == "2019"
+
+def getInfoFromTagsDictByKey_test_date_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Oxxxymiron - Непрожитая жизнь.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    date = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'date')
+
+    assert date == "2021"
+
+def getInfoFromTagsDictByKey_test_genre_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Jimm - Очередной день.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    genre = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'genre')
+
+    assert genre == "Хип-хоп/рэп"
+
+def getInfoFromTagsDictByKey_test_genre_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Machine Gun Kelly - 9 lives.flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    genre = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'genre')
+
+    assert genre == "Альтернативная музыка/инди"
+
+def getInfoFromTagsDictByKey_test_composer_mp3():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Скриптонит - Chapter II.mp3")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    composer = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'composer')
+
+    assert composer == "Разные композиторы"
+
+def getInfoFromTagsDictByKey_test_composer_flac():
+    filepath = os.path.abspath("tests/tagsParsing_tests/dataset/"
+                               "Calvin Harris - Stay With Me (feat. Justin Timberlake, Halsey & Pharrell).flac")
+
+    tagsDict = tagsParsing.getTagsDict(filepath)
+    composer = tagsParsing.getInfoFromTagsDictByKey(tagsDict, 'composer')
+
+    assert composer == "Adam Richard Wiles"
+
+def getInfoFromTagsDictByKey_tests():
+    getInfoFromTagsDictByKey_test_title_mp3()
+    getInfoFromTagsDictByKey_test_title_flac()
+    getInfoFromTagsDictByKey_test_album_mp3()
+    getInfoFromTagsDictByKey_test_album_flac()
+    getInfoFromTagsDictByKey_test_artist_mp3()
+    getInfoFromTagsDictByKey_test_artist_flac()
+    getInfoFromTagsDictByKey_test_albumArtist_mp3()
+    getInfoFromTagsDictByKey_test_albumArtist_flac()
+    getInfoFromTagsDictByKey_test_date_mp3()
+    getInfoFromTagsDictByKey_test_date_flac()
+    getInfoFromTagsDictByKey_test_genre_mp3()
+    getInfoFromTagsDictByKey_test_genre_flac()
+    getInfoFromTagsDictByKey_test_composer_mp3()
+    getInfoFromTagsDictByKey_test_composer_flac()
+
+def tagsParsing_tests():
+    getTagsDict_tests()
+    getInfoFromTagsDictByKey_tests()
