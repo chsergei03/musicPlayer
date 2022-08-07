6f66353f79cb6ee5198f6e7a5fe3ff627c499650
chsergei03 <chsergey1003@gmail.com>
2022-08-07 18:32:10 +0300

    add 'add to filebase' button, getConfiguredButton function, width and height constants to musicPlayer module

--- modules/musicPlayer.py
+++ modules/musicPlayer.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # главное окно музыкального плеера.
 class mainWindow(QMainWindow):
@@ -8,8 +8,25 @@ class mainWindow(QMainWindow):
         super(mainWindow, self).__init__()
 
         # настройка параметров окна:
+        WINDOW_WIDTH, WINDOW_HEIGHT = 400, 400
         self.setWindowTitle("music player")
-        self.setGeometry(0, 0, 400, 400)
+        self.setGeometry(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+        # настройка кнопок:
+        BUTTON_WIDTH, BUTTON_HEIGHT = 400, 50
+        self.buttonAddToFilebase = self.getConfiguredButton(0, 0,
+                                                            BUTTON_WIDTH, BUTTON_HEIGHT,
+                                                            "add to filebase")
+
+    # возвращает кнопку, левый верхний угол которой расположен
+    # на x пунктов правее и на y пунктов ниже левого верхнего
+    # угла экрана; с шириной width, высотой height, подписью text.
+    def getConfiguredButton(self, x, y, width, height, text):
+        button = QPushButton(self)
+        button.setGeometry(x, y, width, height)
+        button.setText(text)
+
+        return button
 
 # запускает приложение музыкального плеера.
 def runApplication():
